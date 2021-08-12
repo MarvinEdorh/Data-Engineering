41b04d43be1c078ec5c83c39c2e5d58944de103b
Marvin EDORH <83826055+MarvinEdorh@users.noreply.github.com>
2021-08-12 02:52:41 +0200

    Update Pipeline_Python_BigQuery.py

--- Pipeline_Python_BigQuery.py
+++ Pipeline_Python_BigQuery.py
@@ -4,11 +4,14 @@ import os; os.chdir('C:/Users/marvin/Desktop/SQL BigQuery')
 
 import numpy as np ; import pandas as pd ; from google.cloud import bigquery
 
-#https://cloud.google.com/docs/authentication/production
+#Création d'un compte de service GCP : https://cloud.google.com/docs/authentication/production
+#Authentification du compte dans Python en ajouant lien du fichier JSON téléchargé en local après la creation de la clé
 
 client = bigquery.Client.from_service_account_json(
 json_credentials_path='C:/Users/marvin/Desktop/SQL BigQuery/data_pipeline-bbc9aec8eae9.json', 
 project='data_pipeline')
+
+#Requête SQL
 
 query = """
 SELECT fullvisitorid AS ID_Visitor,SUM(totals.visits) AS Visits 
@@ -17,6 +20,7 @@ GROUP BY fullvisitorid ORDER BY Visits DESC """
 
 query_results = client.query(query) ; query_results = query_results.result()
 
+#Table des résultats 
 ID_Visitor = [] ; Visits = []
 
 for row in query_results: 
@@ -25,6 +29,10 @@ for row in query_results:
 
 BigQuery_table = { 'ID_Visitor' : ID_Visitor, 'Visits' : Visits } ; BigQuery_table = pd.DataFrame(BigQuery_table)
 
-from pandas.io import gbq
+#Cette table va pouvoir etre soumise à des tests statistiques ou des modèles de machine learning 
+#afin d'effectuer par exemple de l'A/B testing ou des analyses predictives (conf Data-Mining/Machine Learning)
 
+#Après analyses, export des résultats vers Google Cloud Platform BigQuery Storage 
+#afin de mieux les visualiser sur des outils BI de Data Visualisation comme Tableau ou Data Studio
+from pandas.io import gbq
 BigQuery_table.to_gbq(destination_table='test.BigQuery_table', project_id='data_pipeline', if_exists='replace')
